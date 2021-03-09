b5764a9b6303152683e13baec8c7847e1176dc04
Laurianelo <32616344+Laurlo@users.noreply.github.com>
2021-03-09 22:13:20 +0100

    Player's Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using UnityEngine;
+
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 3f;
+
+    [SerializeField]
+    private float mouseSensitivity = 3f;
+
+    [SerializeField]
+    private PlayerMotor motor;
+
+    private float xMov;
+    private float zMov;
+
+    private float yRot;
+    private float xRot;
+
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+
+    private void Update()
+    {
+        MovePlayer();
+
+    }
+
+    private void MovePlayer()
+    {
+        GetInput();
+        //calculate velocity
+        Vector3 moveHorizontal = transform.right * xMov;
+        Vector3 moveVertical = transform.forward * zMov;
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+
+        motor.Move(velocity);
+
+
+        //player rotation left and right
+        yRot = Input.GetAxisRaw("Mouse X");
+        Vector3 rotationLF = new Vector3(0, yRot, 0) * mouseSensitivity;
+        motor.Rotate(rotationLF);
+        /*
+        //player rotation up and down
+        xRot = Input.GetAxisRaw("Mouse X");
+        Vector3 rotationUD = new Vector3(0, xRot, 0) * mouseSensitivity;
+        motor.Rotate(rotationUD);*/
+    }
+
+    private void RotatePlayer()
+    {
+     
+    }
+
+    private void GetInput()
+    {
+        xMov = Input.GetAxisRaw("Horizontal");
+        zMov = Input.GetAxisRaw("Vertical");
+    }
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(PlayerController))]
+public class PlayerMotor : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 velocity;
+    [SerializeField]
+    private Rigidbody rb;
+    [SerializeField]
+    private Vector3 rotation;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+
+    private void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+
+    }
+
+    
+
+    /// <summary>
+    /// Player's rotation
+    /// Camera rotation 
+    /// </summary>
+    private void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+    }
+
+
+ 
+
+    /// <summary>
+    /// Player's movement
+    /// Check if exist a movement before
+    /// </summary>
+    private void PerformMovement()
+    {
+        if(velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+}
